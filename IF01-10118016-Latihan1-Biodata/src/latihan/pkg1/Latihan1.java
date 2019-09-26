f4e46237303a4dc8d670f86cee4e5c78656eb2ba
rezkynurfauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-09-26 12:26:06 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan1-Biodata/src/latihan/pkg1/Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg1;
+
+/**
+ *
+ * Nama : Rezky Nur Fauzi
+ * NIM  : 10118016  
+ * Kelas: IF-1 
+ * Deskripsi Program : Menampilkan Value ke Layar
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Biodata Saya" + "\n" +   "-----------------");
+        System.out.println("NAMA\t\t: Rezky Nur Fauzi");
+        System.out.println("ALAMAT\t\t: Jln. Kubang Utara 2 no 6");
+        System.out.println("TEMPAT LAHIR\t: Ciamis");
+        System.out.println("TANGGAL LAHIR\t: 07 Oktober 1999");
+        System.out.println("-----------------");
+        System.out.println("HOBI\t\t: Games");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Vanilla");
+    }
+    
+}
